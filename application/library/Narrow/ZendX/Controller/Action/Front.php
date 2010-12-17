51dd46b3b8c4305e45b93d492c9726c8746169f6
unknown <toshiba@.(none)>
2010-12-17 14:22:07 +0800

    12345

--- application/library/Narrow/ZendX/Controller/Action/Front.php
+++ application/library/Narrow/ZendX/Controller/Action/Front.php
@@ -14,7 +14,7 @@
 	    	
 			$this->appendJs('js/jquery-1.4.min.js');
 			$this->appendJs('js/rocknoon/include.js');
-//	    	$this->appendJs('js/sms/ajax.js');
+	    	$this->appendJs('js/jquery.textlimit.js');
 //	    	$this->appendJs('js/sms/twitter.js');
 	    }
 	   
