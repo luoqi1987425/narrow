b3e428643fb2ee0a4254468f468abc49bfa2280f
unknown <toshiba@.(none)>
2010-12-20 15:46:51 +0800

    lang support

--- application/library/Narrow/ZendX/Controller/Action.php
+++ application/library/Narrow/ZendX/Controller/Action.php
@@ -11,29 +11,31 @@
 			
 			$translate = Zend_Registry::get( 'translate');
 			
-			$lang_config = array_keys(WeFlex_Application::GetInstance()->getLangs());
+//			$lang_config = array_keys(WeFlex_Application::GetInstance()->getLangs());
+//			
+//			if($this->getRequest()->getParam('lang') && in_array($this->getRequest()->getParam('lang'), $lang_config)){  
+//				@$translate->setLocale( $this->getRequest()->getParam('lang') );
+//				@setcookie('lang', $this->getRequest()->getParam('lang'), time() + 9999999, '/');
+//			}else if($_COOKIE['lang'] && in_array($_COOKIE['lang'], $lang_config)) {
+//				 //cookie
+//				@$translate->setLocale($_COOKIE['lang']);
+//			}        
+//			else if(in_array($translate->getLocale(), $lang_config)){
+//				//browers
+//				@setcookie('lang', $translate->getLocale(), time() + 9999999, '/');
+//			}   
+//			else{ 
+//				//first of list                                                             
+//				@$translate->setLocale(WeFlex_Application::GetInstance()->getDefaultLang());
+//				@setcookie('lang', $translate->getLocale(), time() + 9999999, '/');
+//			}
 			
-			if($this->getRequest()->getParam('lang') && in_array($this->getRequest()->getParam('lang'), $lang_config)){  
-				@$translate->setLocale( $this->getRequest()->getParam('lang') );
-				@setcookie('lang', $this->getRequest()->getParam('lang'), time() + 9999999, '/');
-			}else if($_COOKIE['lang'] && in_array($_COOKIE['lang'], $lang_config)) {
-				 //cookie
-				@$translate->setLocale($_COOKIE['lang']);
-			}        
-			else if(in_array($translate->getLocale(), $lang_config)){
-				//browers
-				@setcookie('lang', $translate->getLocale(), time() + 9999999, '/');
-			}   
-			else{ 
-				//first of list                                                             
-				@$translate->setLocale(WeFlex_Application::GetInstance()->getDefaultLang());
-				@setcookie('lang', $translate->getLocale(), time() + 9999999, '/');
-			}
-			
+			//hack dutch
+			$translate->setLocale("nl_NL");
 			
 			//sepcify the grobal variable for lang
 			$this->_setParam( 'lang'   , $translate->getLocale() );
-			Zend_Registry::set('lang' ,  "nl_NL" );
+			Zend_Registry::set('lang' ,  $translate->getLocale() );
 		}
 
 	}
